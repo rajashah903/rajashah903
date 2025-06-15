a0dc1822520fc3b9c592579b4d31eedf21e61dad
Raja Shah <rajashah52141111@gmail.com>
2025-06-15 21:47:04 +0530

    Add files via upload

---
+++ RAJA PORTFOLIO/script.js
@@ -0,0 +1,39 @@
+ alert('Welcome To My Webside');
+          function myfunction(){
+          var x=document.getElementById("demo2");
+          x.style.color="red";
+          }
+          function myfun(){
+          var x=document.getElementById("h2");
+          x.style.color="red";
+          }
+          function fun(){
+          var x=document.getElementById("edu");
+          x.style.color="red";
+          }
+           function funn(){
+          var x=document.getElementById("DSA");
+          x.style.color="red";
+          }
+           function myfunc(){
+          var x=document.getElementById("proj");
+          x.style.color="red";
+          }
+           function myfunn(){
+          var x=document.getElementById("Tech");
+          x.style.color="red";
+          }
+          function fot(){
+          var x=document.getElementById("fot");
+          x.style.color="red";
+          }
+
+   
+
+
+
+
+
+
+     
+     
